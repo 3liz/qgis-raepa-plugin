150388a7b5b719c969e99d00dceaf47460766044
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-01-27 14:10:52 +0100

    review code in algorithm

--- processing/algorithms/add_styles.py
+++ processing/algorithms/add_styles.py
@@ -9,41 +9,24 @@
  ***************************************************************************/
 """
 
-__author__ = '3liz'
-__date__ = '2019-02-15'
-__copyright__ = '(C) 2019 by 3liz'
-
-# This will get replaced with a git SHA1 when you do a git archive
-
+__copyright__ = 'Copyright 2020, 3Liz'
+__license__ = 'GPL version 3'
+__email__ = 'info@3liz.org'
 __revision__ = '$Format:%H$'
 
 from qgis.core import (
     QgsProcessingAlgorithm,
-    QgsProcessingParameterString,
     QgsProcessingOutputString,
     QgsProcessingParameterVectorLayer,
     QgsProcessingParameterBoolean,
-    QgsExpressionContextUtils,
-    QgsVectorLayer,
-    QgsProcessingOutputMultipleLayers,
-    QgsProcessingContext,
-    QgsApplication,
-    QgsProject,
     QgsProcessing
 )
-from qgis.PyQt.QtSql import *
 from .tools import *
-from processing.tools.postgis import uri_from_name, GeoDB
 import os
 
+
 class AddStyles(QgsProcessingAlgorithm):
-    """
-
-    """
-
-    # Constants used to refer to parameters and outputs. They will be
-    # used when calling the algorithm from another algorithm, or when
-    # calling from the QGIS console.
+    """Add styles algorithm."""
 
     APPARAEP = 'APPARAEP'
     APPARASS = 'APPARASS'
@@ -74,11 +57,6 @@ class AddStyles(QgsProcessingAlgorithm):
         return self.__class__()
 
     def initAlgorithm(self, config):
-        """
-        Here we define the inputs and output of the algorithm, along
-        with some other properties.
-        """
-        # INPUTS
         self.addParameter(
             QgsProcessingParameterVectorLayer(
                 self.APPARAEP,
@@ -135,9 +113,6 @@ class AddStyles(QgsProcessingAlgorithm):
         )
 
 
-
-        # OUTPUTS
-        # Add output for message
         self.addOutput(
             QgsProcessingOutputString(
                 self.OUTPUT_STRING,
@@ -146,16 +121,12 @@ class AddStyles(QgsProcessingAlgorithm):
         )
 
     def processAlgorithm(self, parameters, context, feedback):
-        """
-        Here is where the processing itself takes place.
-        """
         msg = ''
         msgl = ''
-        Sdir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),'..','..','qgis', 'qml')
+        Sdir_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '..', '..', 'qgis', 'qml')
         layer = self.parameterAsVectorLayer(parameters, self.APPARAEP, context)
         style = self.parameterAsBool(parameters, self.STYLETYPE, context)
         if layer.isValid():
-            filename = ''
             if style:
                 fileName = 'actions_appareils_AEP.qml'
             else:
@@ -169,7 +140,6 @@ class AddStyles(QgsProcessingAlgorithm):
 
         layer = self.parameterAsVectorLayer(parameters, self.APPARASS, context)
         if layer.isValid():
-            filename = ''
             if style:
                 fileName = 'actions_appareils_ASS.qml'
             else:
@@ -183,7 +153,6 @@ class AddStyles(QgsProcessingAlgorithm):
 
         layer = self.parameterAsVectorLayer(parameters, self.OUVRAEP, context)
         if layer.isValid():
-            filename = ''
             if style:
                 fileName = 'actions_ouvrages_AEP.qml'
             else:
@@ -197,7 +166,6 @@ class AddStyles(QgsProcessingAlgorithm):
 
         layer = self.parameterAsVectorLayer(parameters, self.OUVRASS, context)
         if layer.isValid():
-            filename = ''
             if style:
                 fileName = 'actions_ouvrages_ASS.qml'
             else:
@@ -211,7 +179,6 @@ class AddStyles(QgsProcessingAlgorithm):
 
         layer = self.parameterAsVectorLayer(parameters, self.CANALAEP, context)
         if layer.isValid():
-            filename = ''
             if style:
                 fileName = 'actions_canalisations_AEP.qml'
             else:
@@ -225,7 +192,6 @@ class AddStyles(QgsProcessingAlgorithm):
 
         layer = self.parameterAsVectorLayer(parameters, self.CANALASS, context)
         if layer.isValid():
-            filename = ''
             if style:
                 fileName = 'actions_canalisation_ASS.qml'
             else:
