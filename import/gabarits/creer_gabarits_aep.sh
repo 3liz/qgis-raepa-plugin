b73e30f23d15ec892fb831f3643967f59e8574a0
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-02-05 15:20:30 +0100

    ZIP pour AEP et ASS en 2154 et 32620

--- import/gabarits/creer_gabarits_aep.sh
+++ import/gabarits/creer_gabarits_aep.sh
@@ -1,15 +1,23 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+then
+  PROJECTION=2154
+else
+  PROJECTION=$1
+fi
 
 # appareils
 # psql service=raepa -c "SELECT column_name FROM information_schema.columns WHERE table_schema = 'raepa' AND table_name = 'raepa_apparass_p' AND column_name NOT LIKE '\_%'"
-ogr2ogr -overwrite -f "ESRI Shapefile" gabarit_appareils_aep.shp PG:"service=raepa" -sql "SELECT idappareil, x, y, mouvrage, gexploit, typreseau, fnappass, andebpose, anfinpose, diametre, idcanamont, idcanaval, idcanppale, idouvrage, z, qualglocxy, qualglocz, datemaj, sourmaj, qualannee, dategeoloc, sourgeoloc, sourattrib, geom FROM raepa.raepa_apparass_p LIMIT 0"
+ogr2ogr -overwrite -f "ESRI Shapefile" -a_srs "EPSG:${PROJECTION}" gabarit_appareils_aep.shp PG:"service=raepa" -sql "SELECT idappareil, x, y, mouvrage, gexploit, typreseau, fnappass, andebpose, anfinpose, diametre, idcanamont, idcanaval, idcanppale, idouvrage, z, qualglocxy, qualglocz, datemaj, sourmaj, qualannee, dategeoloc, sourgeoloc, sourattrib, geom FROM raepa.raepa_apparass_p LIMIT 0"
 
 # canalisations
 # psql service=raepa -c "SELECT column_name FROM information_schema.columns WHERE table_schema = 'raepa' AND table_name   = 'raepa_canalaep_l' AND column_name NOT LIKE '\_%'"
-ogr2ogr -overwrite -f "ESRI Shapefile" gabarit_canalisations_aep.shp PG:"service=raepa" -sql "SELECT idcana, mouvrage, gexploit, enservice, branchemnt, materiau, diametre, anfinpose, modecirc, contcanaep, fonccanaep, idnini, idnterm, idcanppale, profgen, andebpose, longcana, nbranche, qualglocxy, qualglocz, datemaj, sourmaj, qualannee, dategeoloc, sourgeoloc, sourattrib, geom
+ogr2ogr -overwrite -f "ESRI Shapefile" -a_srs "EPSG:${PROJECTION}" gabarit_canalisations_aep.shp PG:"service=raepa" -sql "SELECT idcana, mouvrage, gexploit, enservice, branchemnt, materiau, diametre, anfinpose, modecirc, contcanaep, fonccanaep, idnini, idnterm, idcanppale, profgen, andebpose, longcana, nbranche, qualglocxy, qualglocz, datemaj, sourmaj, qualannee, dategeoloc, sourgeoloc, sourattrib, geom
  FROM raepa.raepa_canalaep_l LIMIT 0"
 
 # ouvrages
 # psql service=raepa -c "SELECT column_name FROM information_schema.columns WHERE table_schema = 'raepa'AND table_name   = 'raepa_ouvraep_p' AND column_name NOT LIKE '\_%'"
-ogr2ogr -overwrite -f "ESRI Shapefile" gabarit_ouvrages_aep.shp PG:"service=raepa" -sql "SELECT idouvrage, x, y, mouvrage, gexploit, fnouvaep, anfinpose, idcanamont, idcanaval, idcanppale, z, andebpose, qualglocxy, qualglocz, datemaj, sourmaj, qualannee, dategeoloc, sourgeoloc, sourattrib, geom FROM raepa.raepa_ouvraep_p LIMIT 0"
+ogr2ogr -overwrite -f "ESRI Shapefile" -a_srs "EPSG:${PROJECTION}" gabarit_ouvrages_aep.shp PG:"service=raepa" -sql "SELECT idouvrage, x, y, mouvrage, gexploit, fnouvaep, anfinpose, idcanamont, idcanaval, idcanppale, z, andebpose, qualglocxy, qualglocz, datemaj, sourmaj, qualannee, dategeoloc, sourgeoloc, sourattrib, geom FROM raepa.raepa_ouvraep_p LIMIT 0"
+
+zip "gabarits_aep_${PROJECTION}".zip gabarit_appareils_aep.* gabarit_ouvrages_aep.* gabarit_canalisations_aep.*
--- import/gabarits/creer_gabarits_ass.sh
+++ import/gabarits/creer_gabarits_ass.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+if [ $# -eq 0 ]
+then
+  PROJECTION=2154
+else
+  PROJECTION=$1
+fi
+
 # appareils
-ogr2ogr -overwrite -f "ESRI Shapefile" gabarit_appareils_ass.shp PG:"service=raepa" -sql "SELECT 'ABCDEF' AS idappareil, '' AS typreseau, '' AS fnappass, 0 AS diametre, 'ABCDEF' AS idouvrage, 0 AS z_appareil, ST_Setsrid(ST_MakePoint(0,0),2154) AS geom FROM raepa.raepa_apparass_p LIMIT 0"
+ogr2ogr -overwrite -f "ESRI Shapefile" -a_srs "EPSG:${PROJECTION}" gabarit_appareils_ass.shp PG:"service=raepa" -sql "SELECT 'ABCDEF' AS idappareil, '' AS typreseau, '' AS fnappass, 0 AS diametre, 'ABCDEF' AS idouvrage, 0 AS z_appareil, ST_Setsrid(ST_MakePoint(0,0),2154) AS geom FROM raepa.raepa_apparass_p LIMIT 0"
 
 # canalisations
-ogr2ogr -overwrite -f "ESRI Shapefile" gabarit_canalisations_ass.shp PG:"service=raepa" -sql "SELECT 'N' AS branchemnt, '' AS typreseau, '' AS materiau, '' AS forme, 0 AS dim_v, 0 AS dim_h, '' AS mode_circ, '' AS fonccanass, '' AS id_nd_aval, '' AS id_nd_amon, 0 AS fe_amont, 0 AS fe_aval, 0 AS nbranche,  ST_Setsrid(ST_MakeLine(ST_MakePoint(0,0),ST_MakePoint(1,1)),2154) AS geom FROM raepa.raepa_canalass_l LIMIT 0"
+ogr2ogr -overwrite -f "ESRI Shapefile" -a_srs "EPSG:${PROJECTION}" gabarit_canalisations_ass.shp PG:"service=raepa" -sql "SELECT 'N' AS branchemnt, '' AS typreseau, '' AS materiau, '' AS forme, 0 AS dim_v, 0 AS dim_h, '' AS mode_circ, '' AS fonccanass, '' AS id_nd_aval, '' AS id_nd_amon, 0 AS fe_amont, 0 AS fe_aval, 0 AS nbranche,  ST_Setsrid(ST_MakeLine(ST_MakePoint(0,0),ST_MakePoint(1,1)),2154) AS geom FROM raepa.raepa_canalass_l LIMIT 0"
 
 # ouvrages
-ogr2ogr -overwrite -f "ESRI Shapefile" gabarit_ouvrages_ass.shp PG:"service=raepa" -sql "SELECT 'ABCDEF' AS idouvrage, '' AS typreseau, '' AS fnouvass, 0 AS z_radier, 0 AS z_tampon, 0 AS angle_tamp, ST_Setsrid(ST_MakePoint(0,0),2154) AS geom FROM raepa.raepa_ouvrass_p LIMIT 0"
+ogr2ogr -overwrite -f "ESRI Shapefile" -a_srs "EPSG:${PROJECTION}" gabarit_ouvrages_ass.shp PG:"service=raepa" -sql "SELECT 'ABCDEF' AS idouvrage, '' AS typreseau, '' AS fnouvass, 0 AS z_radier, 0 AS z_tampon, 0 AS angle_tamp, ST_Setsrid(ST_MakePoint(0,0),2154) AS geom FROM raepa.raepa_ouvrass_p LIMIT 0"
+
+zip "gabarits_ass_${PROJECTION}.zip" gabarit_appareils_ass.* gabarit_ouvrages_ass.* gabarit_canalisations_ass.*
