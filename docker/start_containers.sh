dc312e2e26c3de008e54009de607a02ef5a78685
Etienne Trimaille <gustrimaille@yahoo.fr>
2020-03-05 11:05:35 +0100

    fix some docker containers

--- docker/start_containers.sh
+++ docker/start_containers.sh
@@ -13,8 +13,8 @@ docker exec -it qgis sh -c "qgis_setup.sh raepa"
 docker cp postgis_connections.ini qgis:/tmp
 docker exec qgis bash -c "cat /tmp/postgis_connections.ini >> /root/.local/share/QGIS/QGIS3/profiles/default/QGIS/QGIS3.ini"
 
-echo 'Running tests'
-# docker exec -it qgis sh -c "cd /raepa && qgis_testrunner.sh qgis_plugin_tools.infrastructure.test_runner.test_package"
+# echo 'Running tests'
+# docker exec -it qgis sh -c "cd /tests_directory/raepa && qgis_testrunner.sh qgis_plugin_tools.infrastructure.test_runner.test_package"
 # docker exec qgis qgis
 # docker-compose kill
 # docker-compose rm
