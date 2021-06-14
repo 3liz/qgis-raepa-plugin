e01938a72bd9fd1cd10b9ec515d909f124d8b7ab
ThomasDBM <Thomas.debeaumont@free.fr>
2021-06-14 16:21:12 -0400

    Correction du calcul de l'orientation dans le cas ou l'objet est situé en bout de canalisation

--- raepa/install/sql/raepa/10_FUNCTION.sql
+++ raepa/install/sql/raepa/10_FUNCTION.sql
@@ -34,8 +34,8 @@ BEGIN
             THEN round(CAST(degrees(ST_Azimuth(ST_StartPoint(ST_LineSubstring(cgeom, pos, pos + 0.1)),
         ST_EndPoint(ST_LineSubstring(cgeom, pos, pos + 0.1)))) as numeric), 2)
         WHEN pos >= 0.9
-            THEN round(CAST(degrees(ST_Azimuth(ST_StartPoint(ST_LineSubstring(cgeom, pos, pos)),
-        ST_EndPoint(ST_LineSubstring(cgeom, pos, pos)))) as numeric), 2)
+            THEN round(CAST(degrees(ST_Azimuth(ST_StartPoint(ST_LineSubstring(cgeom, pos - 0.1, pos)),
+        ST_EndPoint(ST_LineSubstring(cgeom, pos - 0.1, pos)))) as numeric), 2)
     END into res
     FROM raepa.raepa_apparaep_p p
     JOIN raepa.raepa_canalaep_l c ON ST_DWithin(c.geom, p.geom, 0.05);
